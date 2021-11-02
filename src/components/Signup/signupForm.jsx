c5ce822c2c3fc2453f22927e4a5e529cac912149
iusharnina@gmail.com <zjulia679@gmail.com>
2021-11-02 13:17:00 +1000

    Added signup

---
+++ src/components/Signup/signupForm.jsx
@@ -0,0 +1,127 @@
+import { React, Fragment, useState, useEffect } from "react";
+import { Grid, Button, Input, InputLabel } from "@material-ui/core";
+import { makeStyles } from "@material-ui/styles";
+import { useHistory } from "react-router-dom";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    margin: "auto",
+    padding: theme.spacing(4),
+    width: "100%",
+  },
+  formGroup: {
+    padding: theme.spacing(2),
+    
+  },
+}));
+
+function SignupForm () {
+    const classes = useStyles();
+    const initialValues = {
+      username: "",
+      email: "",
+      password: ""
+    };
+    const [credentials, setCredentials] = useState(initialValues);
+    const history = useHistory();
+
+    const handleChange = (event) => {
+      const { id, value } = event.target;
+      setCredentials((prevCredentials) => {
+        return {
+        ...prevCredentials,
+        [id]: value,
+      }
+      });
+      console.log(credentials)
+    } 
+
+    const postData = async () => {
+      console.log("I'm postin a user");
+      setCredentials(initialValues)
+      const response = await fetch(
+        `${process.env.REACT_APP_API_URL}users/`,
+        {
+          method: "post",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(credentials)
+           
+          // username: "doggo",
+          // password: "12345678",
+          // email: "doggo@doggo.com"
+        ,
+        }
+      );
+      return response.json();
+    };
+
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      // if (credentials.username && credentials.password && credentials.email) {
+        postData().then((response) => {
+          //window.localStorage.setItem("token", response.token);
+          //history.push("/login");
+          console.log("Response from API---------", response)
+        });
+      // }
+
+    };
+
+return (
+    <Fragment>
+      <Grid container justifyContent="center" spacing={3}>
+        <form  noValidate>
+          <Grid className={classes.formGroup} item xs={12}>
+            <InputLabel className={classes.formLabel} htmlFor="username">
+              Username:
+            </InputLabel>
+            <Input
+              value={credentials.username}
+              className={classes.formInput}
+              type="text"
+              id="username"
+              onChange={handleChange}
+            />
+          </Grid>
+
+          <Grid className={classes.formGroup} item xs={12}>
+            <InputLabel className={classes.formLabel} htmlFor="username">
+              Email:
+            </InputLabel>
+            <Input
+              value={credentials.email}
+              className={classes.formInput}
+              type="text"
+              id="email"
+              
+              onChange={handleChange}
+            />
+          </Grid>
+
+          <Grid className={classes.formGroup} item xs={12}>
+            <InputLabel className={classes.formLabel} htmlFor="password">
+              Password:
+            </InputLabel>
+            <Input
+              value={credentials.password}
+              className={classes.formInput}
+              type="password"
+              id="password"
+              
+              onChange={handleChange}
+            />
+          </Grid>
+          <Grid onClick={handleSubmit} container justifyContent="center">
+            <Button variant="contained" type="submit">
+              Sign up
+            </Button>
+          </Grid>
+        </form>
+      </Grid>
+    </Fragment>
+  );
+}
+
+export default SignupForm;
